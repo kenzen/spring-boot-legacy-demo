1db383195fd10e94b722138432006cada2b9096d
KenZ <ken.zheng@1x3x.net>
2019-06-06 14:46:56 +0800

    fix: refine build scripts

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 set JAVA_HOME=C:\home\dev\java\jdks\jdk1.6.0_45
 set M2_HOME=C:\home\dev\java\tools\apache-maven-3.2.5
 
-%JAVA_HOME%\bin\java.exe -jar .\target\fast-cert-0.0.1-SNAPSHOT.jar
+%JAVA_HOME%\bin\java.exe -jar .\target\spring-boot-legacy-0.0.1-SNAPSHOT.jar
 
 pause
